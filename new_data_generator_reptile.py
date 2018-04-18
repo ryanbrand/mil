fc82613f5b2e61fcf9fe737eb9cde41179d17de0
Jason Krone <jasonkrone@me.com>
2018-04-18 03:35:15 +0000

    new reptile data gen update

--- new_data_generator_reptile.py
+++ new_data_generator_reptile.py
@@ -6,13 +6,15 @@ from data_generator import *
 from natsort import natsorted
 from glob import glob
 
+SIM_PUSH_PATH = '/home/rmb2208/mil/data/sim_push'
+SIM_PUSH_TEST_PATH = '/home/rmb2208/mil/data/sim_push_test'
+SIM_PUSH_SCALE_BIAS_PATH = '/home/rmb2208/mil/data/scale_and_bias_sim_push.pkl'
 
 class MILDataGenerator(object):
-    
+
     # data_gen is the mil data generator
-    def __init__(self, train_dir, test_dir, scale_and_bias_path=None):
-        print('in init')
-        # load scale and bias stats 
+    def __init__(self, train_dir=SIM_PUSH_PATH, test_dir=SIM_PUSH_TEST_PATH, scale_and_bias_path=SIM_PUSH_SCALE_BIAS_PATH):
+        # load scale and bias stats
         if scale_and_bias_path != None:
             with open(scale_and_bias_path, 'rb') as f:
                 scale_bias_dict = pickle.load(f)
@@ -22,11 +24,8 @@ class MILDataGenerator(object):
             self.scale = None
             self.bias  = None
 
-        print('scale:', self.scale.shape, 'bias:', self.bias.shape)
-        print('about to load data')
         self.train_data = self._load_data(train_dir)
-        print('loaded data')
-        #self.test_data  = self._load_data(test_dir)
+        self.test_data  = self._load_data(test_dir)
 
     def _load_data(self, data_dir):
         # get all of the .pkl file paths
@@ -45,11 +44,18 @@ class MILTask(object):
 
     def __init__(self, gif_demo_path, pickle_path, scale=None, bias=None):
         gif_paths = natsorted([os.path.join(gif_demo_path, f) for f in glob(os.path.join(gif_demo_path, '*.gif'))])
-        self.gifs = np.stack([self._read_gif(gif) for gif in gif_paths])
+        self.num_trials = len(gif_paths)
+        self.gifs = self._read_gifs(gif_paths)
+        #print('gifs shape:', self.gifs.shape)
         self.states, self.actions = self._read_pickle(pickle_path, scale, bias)
-        print('gif_demo_path:', gif_demo_path, 'pickle_path:', pickle_path)
-        print('states:', self.states.shape, 'actions:', self.actions.shape, 'gifs:', self.gifs.shape)
+        self.state_dim = self.states.shape[-1]
+        self.action_dim = self.actions.shape[-1]
+        self.gif_dim    = self.gifs.shape[-3:]
+        self.T          = self.states.shape[1]
 
+    # dimension of input to construct model: 20 + 46875 = dim_input
+    # forward called on [update_batch_size, dim_input]
+    # when they do multiple gradient steps they re-use the same data
     def sample(self, num_samples):
         '''
         samples num_trials shots from the task
@@ -58,15 +64,18 @@ class MILTask(object):
                  states: num_samples X 100 X 20, actions: num_samples X 100 X 7
         '''
         # TODO: figure out if each time-step is treated as a seperate example?
-        # get the indexes
+        H, W, C = self.gif_dim
         sample_idxs = np.random.choice(self.num_trials, size=num_samples)
-        gifs = self.gifs[sample_idxs]
-        states = self.states[sample_idxs]
-        actions = self.actions[sample_idxs]
+        gifs = self.gifs[sample_idxs].reshape(num_samples*self.T, -1)
+        states = self.states[sample_idxs].reshape(num_samples*self.T, self.state_dim)
+        actions = self.actions[sample_idxs].reshape(num_samples*self.T, self.action_dim)
         return (gifs, states, actions)
 
-    def _read_gif(self, gif_path):
-        return imageio.mimread(gif_path)
+    def _read_gifs(self, gif_paths):
+        gifs = np.stack([imageio.mimread(gif_path) for gif_path in gif_paths])
+        # move the channel indicies to the front of the images to match format
+        gifs = gifs[:, :, :, :, :3].transpose(0, 1, 4, 3, 2).astype('float32') / 255.0
+        return gifs
 
     def _read_pickle(self, pickle_path, scale, bias):
         # returns a tuple of states and actions where
